b85a9cfe55362f75829881569c5289a2c5d0ac15
tuuguti <tuu_guti@hotmail.com>
2018-05-11 20:53:38 -0300

    Adicionado código do botão Switch e email

--- app/src/main/java/br/com/fiap/app8_passoapasso/ConfiguracoesActivity.java
+++ app/src/main/java/br/com/fiap/app8_passoapasso/ConfiguracoesActivity.java
@@ -6,10 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
-    private Button concluirConfiguracoes;
-
     private Button concluirConfiguracoes;
     private Switch ligar;
     private EditText email;
@@ -17,24 +16,29 @@ public class ConfiguracoesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_configuracoes);
+
         ligar = findViewById(R.id.ligar);
         email = findViewById(R.id.email);
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        preferences.getBoolean("ligar",false);
 
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+
+        ligar.setChecked(preferences.getBoolean("ligar", false));
+        email.setText(preferences.getString("email",""));
 
         concluirConfiguracoes = findViewById(R.id.concluirConfiguracoes);
         concluirConfiguracoes.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+                editor.putBoolean("ligar",ligar.isChecked());
+                editor.putString("email",email.getText().toString());
+                editor.apply();
                 finish();
             }
         });
 
-        public void onClick(View v){
-            finish();
-    }
     }
 
 }
