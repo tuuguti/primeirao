3a283c0e55b7942fd18ea1a17f48462b365ef033
tuuguti <tuu_guti@hotmail.com>
2018-05-11 20:39:28 -0300

    Commit inutil, é feio, mas é necessário

--- app/src/main/java/br/com/fiap/app8_passoapasso/ConfiguracoesActivity.java
+++ app/src/main/java/br/com/fiap/app8_passoapasso/ConfiguracoesActivity.java
@@ -1,17 +1,28 @@
 package br.com.fiap.app8_passoapasso;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
     private Button concluirConfiguracoes;
+
+    private Button concluirConfiguracoes;
+    private Switch ligar;
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+        ligar = findViewById(R.id.ligar);
+        email = findViewById(R.id.email);
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        preferences.getBoolean("ligar",false);
+
 
         concluirConfiguracoes = findViewById(R.id.concluirConfiguracoes);
         concluirConfiguracoes.setOnClickListener(new View.OnClickListener() {
@@ -20,6 +31,10 @@ public class ConfiguracoesActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        public void onClick(View v){
+            finish();
+    }
     }
 
 }
